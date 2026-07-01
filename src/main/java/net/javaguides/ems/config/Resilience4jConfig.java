7d4750257514883f2abc74652f4d7eddedb10563
YuHsuan(Krystal)Yang <yuh2636@gmail.com>
2026-07-01 01:11:46 -0400

    feature_circuitbreaker add Resilience4j

---
+++ src/main/java/net/javaguides/ems/config/Resilience4jConfig.java
@@ -0,0 +1,8 @@
+package net.javaguides.ems.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Resilience4jConfig {
+  // Circuit breaker and retry settings are in application.properties under resilience4j.*
+}
