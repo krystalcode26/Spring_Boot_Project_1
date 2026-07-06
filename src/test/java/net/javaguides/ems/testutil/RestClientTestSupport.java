a3965d61ee3f4600e8208413e363cc37a57dfb2c
YuHsuan(Krystal)Yang <yuh2636@gmail.com>
2026-07-06 17:29:57 -0400

    feature_test3 update applications.properties, pom.xml, jenkinsfile

--- src/test/java/net/javaguides/ems/testutil/RestClientTestSupport.java
+++ src/test/java/net/javaguides/ems/testutil/RestClientTestSupport.java
@@ -14,31 +14,22 @@ public final class RestClientTestSupport {
 
   @SuppressWarnings("unchecked")
   public static void stubPostBody(RestClient restClient, Object responseBody) {
-    RestClient.RequestBodyUriSpec uriSpec = mock(RestClient.RequestBodyUriSpec.class);
-    RestClient.RequestBodySpec bodySpec = mock(RestClient.RequestBodySpec.class);
-    RestClient.ResponseSpec responseSpec = mock(RestClient.ResponseSpec.class);
-
-    when(restClient.post()).thenReturn(uriSpec);
-    when(uriSpec.uri(anyString())).thenReturn(bodySpec);
-    when(bodySpec.body(any(Object.class))).thenReturn(bodySpec);
-    when(bodySpec.retrieve()).thenReturn(responseSpec);
+    RestClient.ResponseSpec responseSpec = stubPostChain(restClient);
     when(responseSpec.body(any(Class.class))).thenReturn(responseBody);
   }
 
   @SuppressWarnings("unchecked")
   public static void stubPostFailure(RestClient restClient, RuntimeException exception) {
-    RestClient.RequestBodyUriSpec uriSpec = mock(RestClient.RequestBodyUriSpec.class);
-    RestClient.RequestBodySpec bodySpec = mock(RestClient.RequestBodySpec.class);
-    RestClient.ResponseSpec responseSpec = mock(RestClient.ResponseSpec.class);
-
-    when(restClient.post()).thenReturn(uriSpec);
-    when(uriSpec.uri(anyString())).thenReturn(bodySpec);
-    when(bodySpec.body(any(Object.class))).thenReturn(bodySpec);
-    when(bodySpec.retrieve()).thenReturn(responseSpec);
+    RestClient.ResponseSpec responseSpec = stubPostChain(restClient);
     when(responseSpec.body(any(Class.class))).thenThrow(exception);
   }
 
   public static void stubPostBodiless(RestClient restClient) {
+    RestClient.ResponseSpec responseSpec = stubPostChain(restClient);
+    when(responseSpec.toBodilessEntity()).thenReturn(ResponseEntity.ok().build());
+  }
+
+  private static RestClient.ResponseSpec stubPostChain(RestClient restClient) {
     RestClient.RequestBodyUriSpec uriSpec = mock(RestClient.RequestBodyUriSpec.class);
     RestClient.RequestBodySpec bodySpec = mock(RestClient.RequestBodySpec.class);
     RestClient.ResponseSpec responseSpec = mock(RestClient.ResponseSpec.class);
@@ -47,6 +38,6 @@ public final class RestClientTestSupport {
     when(uriSpec.uri(anyString())).thenReturn(bodySpec);
     when(bodySpec.body(any(Object.class))).thenReturn(bodySpec);
     when(bodySpec.retrieve()).thenReturn(responseSpec);
-    when(responseSpec.toBodilessEntity()).thenReturn(ResponseEntity.ok().build());
+    return responseSpec;
   }
 }
